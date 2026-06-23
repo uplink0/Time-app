f23346637c8cf01352482a5d38aa8bf524a604cb
uplink0 <iks_o@inbox.ru>
2026-06-23 10:52:34 +0300

    Update README

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
+  },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
