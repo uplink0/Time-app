8f283f1d36a61e2277abf7cb6fd75f91e1453451
uplink0 <iks_o@inbox.ru>
2026-06-30 14:27:50 +0300

    Use ConfigMap and Secret for API configuration

--- api/src/utils/mysqlPool.mjs
+++ api/src/utils/mysqlPool.mjs
@@ -2,11 +2,11 @@ import mysql from 'mysql2'
 
 const pool = mysql.createPool({
   connectionLimit: 100,
-  host: 'mysql',
-  port: '3306',
-  user: 'root',
-  password: 'password',
-  database: 'time_db',
+  host: process.env.MYSQL_HOST || 'mysql',
+  port: process.env.MYSQL_PORT || '3306',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'time_db',
 })
 
 const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
